5a4143b91a928463005f5344d719620632890641
arpan das <admin@27jb48.onmicrosoft.com>
2022-02-20 19:14:43 +0530

    print "MS" print folder of e=window(ICON)

---
+++ msprn.py
@@ -0,0 +1,15 @@
+def msprn():
+    print("|||          |||       ||||")
+    print("||||        ||||      ||  ||")
+    print("|| ||      || ||     ||    ||")
+    print("||  ||    ||  ||      ||")
+    print("||   ||  ||   ||       ||")
+    print("||    ||||    ||        ||")
+    print("||            ||         ||")
+    print("||            ||  ||    ||")
+    print("||            ||   ||  ||")
+    print("||            ||    ||||")
+    return 0
+
+msprn()
+input("exit")
